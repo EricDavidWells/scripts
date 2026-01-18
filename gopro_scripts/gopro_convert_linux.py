990efcdddc4f21f733ad8bcb2c1cb1ff99921502
eric.wells <ericwellsrobotics@gmail.com>
2026-01-18 09:49:38 -0800

    minor tweaks

--- gopro_scripts/gopro_convert_linux.py
+++ gopro_scripts/gopro_convert_linux.py
@@ -15,6 +15,12 @@ def parse_arguments():
         help='The source directory'
     )
     parser.add_argument('--rewrite-existing', action='store_true')
+    parser.add_argument(
+        '--video-regex',
+        type=str,
+        default=r'\.mp4$',
+        help=r"Regex to match input video files (default: '\.mp4$'). Example: '\.(mp4|mov)$'"
+    )
 
     # Parse the arguments
     return parser.parse_args()
@@ -26,7 +32,7 @@ if __name__ == "__main__":
     # Print the arguments (or do something with them)
     print(f"Source Directory: {args.source_dir}")
 
-    video_pattern = re.compile(r'\.mp4', re.IGNORECASE)
+    video_pattern = re.compile(args.video_regex, re.IGNORECASE)
 
     for root, subdirs, files in os.walk(args.source_dir):
         for file in files:
@@ -36,8 +42,7 @@ if __name__ == "__main__":
 
             file_root, file_ext = os.path.splitext(file_path)
             output_path = os.path.join(root, f"{file_root}.mov")
+            cmd = f"ffmpeg -i {file_path} -c:v mpeg4 -q:v 1 -vf scale=in_range=pc:out_range=tv -c:a pcm_s16le {output_path} {'-y' if args.rewrite_existing else ''}"
+            print(cmd)
             if os.path.exists(output_path) and not args.rewrite_existing: continue
-
-            
-
-            subprocess.call([f"ffmpeg -i {file_path} -c:v mpeg4 -q:v 1 -c:a pcm_s16le {output_path} {'-y' if args.rewrite_existing else ''}"], shell=True)
+            subprocess.call([f"ffmpeg -i {file_path} -c:v mpeg4 -q:v 1 -vf scale=in_range=pc:out_range=tv -c:a pcm_s16le {output_path} {'-y' if args.rewrite_existing else ''}"], shell=True)
